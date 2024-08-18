3d3b9f61e8c9e94785d938400035d62ea903f593
houheya <127867059+hehetoshang@users.noreply.github.com>
2024-08-18 11:53:42 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-console.log('%cCopyright © 2024 zyyo.net', 'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;');
+console.log('%cCopyright © 2024 houheya.us.kg', 'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;');
 console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
 console.log('%c  ( o.o )', 'color: #8B4513; font-size: 20px;');
 console.log(' %c  > ^ <', 'color: #8B4513; font-size: 20px;');
